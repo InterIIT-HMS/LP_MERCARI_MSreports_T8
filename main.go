d775a93d080abba31b83cdde453edca2ae96713e
Shubham Gupta <shubham.gupta2956@gmail.com>
2022-03-24 03:27:15 +0530

    Integrate sentry
    Signed-off-by: Shubham Gupta <shubham.gupta2956@gmail.com>

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"github.com/wryonik/appointment/controllers"
 	"github.com/wryonik/appointment/models"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://87cb609bebb8450283fb75d18f14aa28@o1176298.ingest.sentry.io/6273809",
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+
 	r := gin.Default()
 
 	// Connect to database
